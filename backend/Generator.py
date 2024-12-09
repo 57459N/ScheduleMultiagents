315cf6a602e527d05a2044aef4b110ac60605817
avantyrbIst <maksimtorpeda74@gmail.com>
2024-12-09 22:12:26 +0300

    feat. rebuild Generator

--- backend/Generator.py
+++ backend/Generator.py
@@ -1,231 +1,128 @@
+from entities import *
+import pandas as pd
 import json
-import csv
-
-# Загрузка данных из JSON файла
-with open('data.json', 'r', encoding='utf-8') as file:
-    input_data = json.load(file)
-
-lessons = input_data['lessons']
-teachers = input_data['teachers']
-#todo записки сумасшедшего 4 час ночи - мы создаём буферку-словарь, где ключ - id группы, значение список занятий у этой группы
-dict_group = {
-    '1': [],
-    '2': [],
-    '3': [],
-    '4': [],
-    '5': [],
-    '6': [],
-    '7': [],
-    '8': [],
-    '9': [],
-    '10': [],
-    '11': [],
-    '12': [],
-    '13': [],
-    '14': [],
-    '15': [],
-    '16': []
-}
-
-#todo эта шляпа для норм работы с данными, здесь мы работаем с ключом - id группы, значение - предметы для этой группы
-def fill_dict_group(dict_group, lesson):
-    lesson_id = lesson['id']
-    subject_name = lesson['subject']['name']
-    lesson_type = lesson['type']
-    teacher_id = lesson['teacher']
-
-    for group in lesson['group']:
-        group_id = str(group['group_id'])
-        if group_id in dict_group:
-            dict_group[group_id].append({
-                'lesson_id': lesson_id,
-                'subject': subject_name,
-                'type': lesson_type,
-                'teacher': teacher_id
-            })
-
-def find_teacher(teachers, id):
-    for teacher in teachers:
-        if teacher['id'] == id:
-            return teacher['id']
+from constants import *
 
 
-# todo эта шляпа - неделя для всех групп, ключ - id_группы, значение - ещё один словарь в котором ключ - id дня а значение - пары(занятия), которые стоят в этот день
-dict_table = {
-        '1': {
-            '1': [],
-            '2': [],
-            '3': [],
-            '4': [],
-            '5': [],
-            '6': []
-        },
-        '2': {
-            '1': [],
-            '2': [],
-            '3': [],
-            '4': [],
-            '5': [],
-            '6': []
-        },
-        '3': {
-            '1': [],
-            '2': [],
-            '3': [],
-            '4': [],
-            '5': [],
-            '6': []
-        },
-        '4': {
-            '1': [],
-            '2': [],
-            '3': [],
-            '4': [],
-            '5': [],
-            '6': []
-        },
-        '5': {
-            '1': [],
-            '2': [],
-            '3': [],
-            '4': [],
-            '5': [],
-            '6': []
-        },
-        '6': {
-            '1': [],
-            '2': [],
-            '3': [],
-            '4': [],
-            '5': [],
-            '6': []
-        },
-        '7': {
-            '1': [],
-            '2': [],
-            '3': [],
-            '4': [],
-            '5': [],
-            '6': []
-        },
-        '8': {
-            '1': [],
-            '2': [],
-            '3': [],
-            '4': [],
-            '5': [],
-            '6': []
-        },
-        '9': {
-            '1': [],
-            '2': [],
-            '3': [],
-            '4': [],
-            '5': [],
-            '6': []
-        },
-        '10': {
-            '1': [],
-            '2': [],
-            '3': [],
-            '4': [],
-            '5': [],
-            '6': []
-        },
-        '11': {
-            '1': [],
-            '2': [],
-            '3': [],
-            '4': [],
-            '5': [],
-            '6': []
-        },
-        '12': {
-            '1': [],
-            '2': [],
-            '3': [],
-            '4': [],
-            '5': [],
-            '6': []
-        },
-        '13': {
-            '1': [],
-            '2': [],
-            '3': [],
-            '4': [],
-            '5': [],
-            '6': []
-        },
-        '14': {
-            '1': [],
-            '2': [],
-            '3': [],
-            '4': [],
-            '5': [],
-            '6': []
-        },
-        '15': {
-            '1': [],
-            '2': [],
-            '3': [],
-            '4': [],
-            '5': [],
-            '6': []
-        },
-        '16': {
-            '1': [],
-            '2': [],
-            '3': [],
-            '4': [],
-            '5': [],
-            '6': []
-        }
-}
-for lesson in lessons:
-    fill_dict_group(dict_group, lesson)
-print(dict_group)
-print(dict_group['1'][0]['teacher'])
+class Generator():
+    def __init__(self, lessons, teachers):
+        self.lessons = lessons
+        self.teachers = teachers
 
-def choose_lecture(dict_group, teachers, dict_table):
-    for group_key, lessons in dict_group.items():
-        for lesson in lessons:
-            if lesson['type'] == "Лекция":
-                teacher_id = lesson['teacher']
-                teacher_schedule = next(
-                    (t['schedule'] for t in teachers if t['id'] == teacher_id), None
-                )
-                if not teacher_schedule:
-                    continue
-                for day in dict_table[group_key]:
-                    day_index = int(day) - 1
-                    if (
-                            isinstance(dict_table[group_key][day], list)
-                            and len(dict_table[group_key][day]) < 5
-                            and day_index < len(teacher_schedule)
-                            and lesson['lesson_id'] in teacher_schedule[day_index]
-                    ):
-                        dict_table[group_key][day].append(lesson['lesson_id'])
-                        break
+        self.dict_group = {str(i): [] for i in range(1, 17)}
+        self.dict_table = {str(i): {str(j): [] for j in range(1, 7)} for i in range(1, 17)}
 
-# на выходе - заполненный словарь dict_table, в котором ключ - id_group, значение - словарь, в котором ключ - day, значение - id_lesson
-# матюша, не бей палкой пж
-# по идее, нельзя заполнять в одном цикле лабы и лекции,
-# тогда лабы будут накладываться в один день у других групп(лаб1 у 1-ой группы и лаб1 у 4-ой группы в одно время в один день) либо я осёл
-# (чек пример матюши)
-#todo или в начале заполнить лабы а потом начать их менять
-# def check_labs_intersections():
-# я доделаю
+    def fill_dict_group(self, lesson):
+        lesson_id = lesson['id']
+        subject_name = lesson['subject']['name']
+        lesson_type = lesson['type']
+        teacher_id = lesson['teacher']
 
+        for group in lesson['group']:
+            group_id = str(group['group_id'])
+            if group_id in self.dict_group:
+                self.dict_group[group_id].append({
+                    'lesson_id': lesson_id,
+                    'subject': subject_name,
+                    'type': lesson_type,
+                    'teacher': teacher_id
+                })
 
+    def find_teacher(self, teacher_id):
+        for teacher in self.teachers:
+            if teacher['id'] == teacher_id:
+                return teacher['id']
+        return None
 
-choose_lecture(dict_group, teachers, dict_table)
+    def choose_lecture(self):
+        for group_key, lessons in self.dict_group.items():
+            for lesson in lessons:
+                if lesson['type'] == "Лекция":
+                    teacher_id = lesson['teacher']
+                    teacher_schedule = next(
+                        (t['schedule'] for t in self.teachers if t['id'] == teacher_id), None
+                    )
+                    if not teacher_schedule:
+                        continue
+                    for day in self.dict_table[group_key]:
+                        day_index = int(day) - 1
+                        if (
+                                isinstance(self.dict_table[group_key][day], list)
+                                and len(self.dict_table[group_key][day]) < 5
+                                and day_index < len(teacher_schedule)
+                                and lesson['lesson_id'] in teacher_schedule[day_index]
+                        ):
+                            self.dict_table[group_key][day].append(lesson['lesson_id'])
+                            break
 
-for i in range(1, len(dict_table) + 1):
-    print(dict_table[str(i)])
+    def choose_labs(self):
+        for group_key, lessons in self.dict_group.items():
+            for lesson in lessons:
+                if lesson['type'] == "Лабораторные":
+                    for day in self.dict_table[group_key]:
+                        day_index = int(day) - 1
+                        if (
+                                isinstance(self.dict_table[group_key][day], list) and
+                                len(self.dict_table[group_key][
+                                        day]) < 5 and  # Убедимся, что в день еще можно добавить занятие
+                                day_index < len(self.teachers[0]['schedule'])  # Проверка на корректность расписания
+                        ):
+                            # Проверяем, не занят ли этот день для лабораторных других групп
+                            is_conflict = False
+                            for other_group_key in self.dict_table:
+                                if other_group_key != group_key:  # Не проверяем саму группу
+                                    # Если в этот день уже есть лабораторная работа, пересечение
+                                    if lesson['lesson_id'] in self.dict_table[other_group_key][day]:
+                                        is_conflict = True
+                                        break
+                            if not is_conflict:
+                                self.dict_table[group_key][day].append(lesson['lesson_id'])
+                                break
 
-csv_file = "output.csv"
-with open(csv_file, mode='w', newline='') as file:
-    writer = csv.writer(file)
-    writer.writerow(dict_table.keys())
-    writer.writerow(dict_table.values())
-print(f"CSV файл '{csv_file}' успешно создан!")
+    def generate_schedule(self):
+        for lesson in self.lessons:
+            self.fill_dict_group(lesson)
+
+        self.choose_lecture()
+
+        self.choose_labs()
+
+    def to_csv(self, path):
+        # Определим список дней недели и групп
+        WEEK_DAYS = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
+        GROUPS = [str(i) for i in range(1, 17)]  # Группы от 1 до 16
+
+        days = []
+        for _ in range(6):  # 6 дней недели
+            for _ in range(8):  # 8 занятий в день
+                days.append(_)
+
+        # Подготовим список занятий по дням для всех групп
+        schedule_data = []
+
+        # Перебираем все группы
+        for group in GROUPS:
+            group_schedule = []
+            for day in range(1, 7):  # Для каждого дня недели
+                day_schedule = self.dict_table[group].get(str(day), [])
+                # Заполняем ячейку дня группы занятием (если оно есть, иначе оставляем пустым)
+                group_schedule.extend(
+                    [lesson.get('subject', '') for lesson in day_schedule] + [''] * (8 - len(day_schedule)))
+
+            schedule_data.append(group_schedule)
+
+        # Создаем DataFrame из данных
+        schedule = pd.DataFrame(schedule_data, columns=[f'Занятие {i + 1}' for i in range(8)] * 6, index=GROUPS)
+
+        # Преобразуем в долгий формат для сохранения в CSV
+        schedule = schedule.transpose().reset_index()
+        schedule.columns = ['День недели'] + [f'Группа {i}' for i in range(1, 17)]
+
+        # Сохраняем в CSV
+        schedule.to_csv(path, sep=';', index=False)
+
+        # Вывод отладочной информации
+        print(schedule)
+        print(schedule.count().sum() - 48 * 2)
+        print(f"CSV файл '{path}' успешно создан!")
+
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from ScheduleGenerator import *
+from Generator import *
 from SheetFormater import *
 
 # ? ONLY FOR DEBUG
@@ -9,14 +10,14 @@ def main():
     data = json.load(f)
     f.close()
     lessons = Lessons(**data)
-    gen = ScheduleGenerator(lessons.lessons, lessons.teachers)
-    gen.generate()
-    gen.to_csv('data/example-2.csv')
+    gen = Generator(lessons.lessons, lessons.teachers)
+    gen.generate_schedule()
+    gen.to_csv('data/example-3.csv')
 
-    formatter = SheetFormater('data/data.json', 'data/example-2.csv')
+    formatter = SheetFormater('data/data.json', 'data/example-3 .csv')
     formatter.format()
-    formatter.save('test2.xlsx')
-    subprocess.Popen('open test2.xlsx', shell=True)
+    formatter.save('test3.xlsx')
+    subprocess.Popen('open test3.xlsx', shell=True)
 
 
 if __name__ == '__main__':
