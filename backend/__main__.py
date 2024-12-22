b4ff7183e92c97a03cfd80e68b227543aaf4d73f
57459N <stasun008@gmail.com>
2024-12-23 02:01:34 +0300

    try to rebuild docker and run with `uvicorn main:app`

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,24 +1,23 @@
-from backend.app.ScheduleGenerator import *
-from backend.app.Generator import *
-from backend.app.SheetFormater import *
+import json
 
-# ? ONLY FOR DEBUG
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pydantic import BaseModel
 
-def main():
-    f = open('./backend/app/data/data.json', 'r')
-    data = json.load(f)
-    print(data)
+from backend.app.entities import *
+
+
+app = FastAPI()
+
+
+@app.post('/')
+async def get_data(lessons: Lessons):
+    f = open('data/export_data.json', 'w')
+    f.write(lessons.model_dump_json())
     f.close()
-    lessons = Lessons(**data)
-    gen = Generator(lessons.lessons, lessons.teachers)
-    gen.generate_schedule()
-    gen.to_csv('./backend/app/data/example-3.csv')
-
-    formatter = SheetFormater('./backend/app/data/data.json', 'backend/app/data/example-3.csv')
-    formatter.format()
-    formatter.save('test3.xlsx')
-    subprocess.Popen('open test3.xlsx', shell=True)
 
 
-if __name__ == '__main__':
-    main()
+@app.get("/file/download")
+def download_file():
+    # for test
+    return FileResponse(path='data/Расписание 4 курс 7 семестр 2024-2025.xlsx', filename='Расписание.xlsx', media_type='multipart/form-data')
--- backend/app/Generator.py
+++ backend/app/Generator.py
@@ -1,16 +1,21 @@
 from backend.app.entities import *
+import pprint
 import pandas as pd
-import json
-from backend.app.constants import *
-
 
 class Generator():
-    def __init__(self, lessons, teachers):
+    def __init__(self, lessons: list[Lesson], teachers: list[Teacher]):
         self.lessons = lessons
         self.teachers = teachers
 
         self.dict_group = {str(i): [] for i in range(1, 17)}
         self.dict_table = {str(i): {str(j): [] for j in range(1, 7)} for i in range(1, 17)}
+        self.teacher_busy = {}
+        for t in teachers:
+            # day_str от "1" до "6"
+            self.teacher_busy[t.id] = {str(day): set() for day in range(1, 7)}
+
+    def print_dict_group(self):
+        pprint.pprint(self.dict_group)
 
     def fill_dict_group(self, lesson):
         lesson_id = lesson.id
@@ -37,45 +42,115 @@ class Generator():
     def choose_lecture(self):
         for group_key, lessons in self.dict_group.items():
             for lesson in lessons:
-                if lesson.get("type") == "Лекция":
-                    teacher_id = lesson.get("type")
+                if lesson['type'] == "Лекция":
+                    teacher_id = lesson['teacher']
                     teacher_schedule = next(
                         (t.schedule for t in self.teachers if t.id == teacher_id), None
                     )
+
                     if not teacher_schedule:
+                        print(f"Преподаватель #{teacher_id} не имеет расписания. Пропускаем...")
                         continue
-                    for day in self.dict_table[group_key]:
-                        day_index = int(day) - 1
+
+                    placed = False
+
+                    for day_str in self.dict_table[group_key]:
+                        day_index = int(day_str) - 1
+
                         if (
-                                isinstance(self.dict_table[group_key][day], list)
-                                and len(self.dict_table[group_key][day]) < 5
+                                len(self.dict_table[group_key][day_str]) < 5
                                 and day_index < len(teacher_schedule)
-                                and lesson.id in teacher_schedule[day_index]
+                                and teacher_schedule[day_index]
                         ):
-                            self.dict_table[group_key][day].append(lesson.id)
+                            free_pair = teacher_schedule[day_index][0]
+                            if free_pair in self.teacher_busy[teacher_id][day_str]:
+                                print(f"Конфликт! Преподаватель {teacher_id} уже занят "
+                                      f"в день {day_str}, пару {free_pair}. Пробуем следующий день...")
+                                continue
+                            self.dict_table[group_key][day_str].append({
+                                'lesson_id': lesson['lesson_id'],
+                                'subject': lesson['subject'],
+                                'type': lesson['type'],
+                                'pair': free_pair,
+                                'teacher': teacher_id
+                            })
+
+                            teacher_schedule[day_index].remove(free_pair)
+
+                            self.teacher_busy[teacher_id][day_str].add(free_pair)
+
+                            placed = True
+                            print(f"Поставили Лекции #{lesson['lesson_id']} (гр. {group_key}) "
+                                  f"на день {day_str} (пара {free_pair}).")
                             break
+                        else:
+                            print(f"Лекции #{lesson['lesson_id']} (гр. {group_key}) "
+                                  f"в день {day_str} не подходят (преп. {teacher_id}). "
+                                  f"Пробуем следующий день...")
+                            continue
+
+                    if not placed:
+                        print(
+                            f"Не удалось поставить Лабораторные #{lesson['lesson_id']} (гр. {group_key})."
+                        )
 
     def choose_labs(self):
         for group_key, lessons in self.dict_group.items():
             for lesson in lessons:
-                if lesson.get("type") == "Лабораторные":
-                    for day in self.dict_table[group_key]:
-                        day_index = int(day) - 1
+                if lesson['type'] == "Лабораторные":
+                    teacher_id = lesson['teacher']
+                    teacher_schedule = next(
+                        (t.schedule for t in self.teachers if t.id == teacher_id), None
+                    )
+
+                    if not teacher_schedule:
+                        print(f"Преподаватель #{teacher_id} не имеет расписания. Пропускаем...")
+                        continue
+
+                    placed = False
+
+                    for day_str in self.dict_table[group_key]:
+                        day_index = int(day_str) - 1
+
                         if (
-                                isinstance(self.dict_table[group_key][day], list) and
-                                len(self.dict_table[group_key][
-                                        day]) < 5 and
-                                day_index < len(self.teachers[0].schedule)
+                                len(self.dict_table[group_key][day_str]) < 5
+                                and day_index < len(teacher_schedule)
+                                and teacher_schedule[day_index]
                         ):
-                            is_conflict = False
-                            for other_group_key in self.dict_table:
-                                if other_group_key != group_key:
-                                    if lesson.get("id") in self.dict_table[other_group_key][day]:
-                                        is_conflict = True
-                                        break
-                            if not is_conflict:
-                                self.dict_table[group_key][day].append(lesson.get("id"))
-                                break
+                            free_pair = teacher_schedule[day_index][0]
+
+                            if free_pair in self.teacher_busy[teacher_id][day_str]:
+                                print(f"Конфликт! Преподаватель {teacher_id} уже занят "
+                                      f"в день {day_str}, пару {free_pair}. Пробуем следующий день...")
+                                continue
+
+                            self.dict_table[group_key][day_str].append({
+                                'lesson_id': lesson['lesson_id'],
+                                'subject': lesson['subject'],
+                                'type': lesson['type'],
+                                'pair': free_pair,
+                                'teacher': teacher_id
+                            })
+
+                            teacher_schedule[day_index].remove(free_pair)
+
+                            self.teacher_busy[teacher_id][day_str].add(free_pair)
+
+                            placed = True
+                            print(f"Поставили Лабы #{lesson['lesson_id']} (гр. {group_key}) "
+                                  f"на день {day_str} (пара {free_pair}).")
+                            break
+                        else:
+                            print(f"Лабы #{lesson['lesson_id']} (гр. {group_key}) "
+                                  f"в день {day_str} не подходят (преп. {teacher_id}). "
+                                  f"Пробуем следующий день...")
+                            continue
+
+                    if not placed:
+                        print(
+                            f"Не удалось поставить Лабораторные #{lesson['lesson_id']} (гр. {group_key})."
+                        )
+
 
     def generate_schedule(self):
         for lesson in self.lessons:
@@ -85,34 +160,92 @@ class Generator():
 
         self.choose_labs()
 
-    def to_csv(self, path):
+    def print_schedule(self):
         WEEK_DAYS = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота']
-        GROUPS = [str(i) for i in range(1, 17)]  # Группы от 1 до 16
 
-        days = []
-        for _ in range(6):  # 6 дней недели
-            for _ in range(8):  # 8 занятий в день
-                days.append(_)
+        for group_key, schedule in self.dict_table.items():
+            print(f"Группа {group_key}:")
+            for day_index, lessons in schedule.items():
+                day_name = WEEK_DAYS[int(day_index) - 1]
+                print(f"Lessons: {lessons}")
+                lesson_info = ", ".join([lesson.get('subject', 'Неизвестный предмет') for lesson in lessons])
+                print(f"  {day_name}: {lesson_info if lesson_info else 'Нет занятий'}")
+            print("-" * 40)
 
-        schedule_data = []
+    def schedule_to_csv(self, filename="schedule.csv"):
 
-        for group in GROUPS:
-            group_schedule = []
-            for day in range(1, 7):  # Для каждого дня недели
-                day_schedule = self.dict_table[group].get(str(day), [])
-                group_schedule.extend(
-                    [lesson.get('subject', '') for lesson in day_schedule if lesson is not None] + [''] * (8 - len(day_schedule)))
+        columns = [
+            "day",
+            "lesson",
+            "1 РФ", "2 РФ", "3 РФ", "4 РФ", "8 РФ", "3 ФЭ", "2 АРИСТ", "8 АРИСТ",
+            "4 КБ", "5 КБ", "6 КБ", "7 КБ", "1 ПИ", "5 ПИ", "6 ПИ", "7 ПИ"
+        ]
 
-            schedule_data.append(group_schedule)
+        GROUP_COLUMN_NAME = {
+            "1": "1 РФ",
+            "2": "2 РФ",
+            "3": "3 РФ",
+            "4": "4 РФ",
+            "5": "8 РФ",
+            "6": "3 ФЭ",
+            "7": "2 АРИСТ",
+            "8": "8 АРИСТ",
+            "9": "4 КБ",
+            "10": "5 КБ",
+            "11": "6 КБ",
+            "12": "7 КБ",
+            "13": "1 ПИ",
+            "14": "5 ПИ",
+            "15": "6 ПИ",
+            "16": "7 ПИ",
+        }
 
-        schedule = pd.DataFrame(schedule_data, columns=[f'Занятие {i + 1}' for i in range(8)] * 6, index=GROUPS)
+        WEEK_DAYS = {
+            "1": "ПОНЕДЕЛЬНИК",
+            "2": "ВТОРНИК",
+            "3": "СРЕДА",
+            "4": "ЧЕТВЕРГ",
+            "5": "ПЯТНИЦА",
+            "6": "СУББОТА"
+        }
 
-        schedule = schedule.transpose().reset_index()
-        schedule.columns = ['День недели'] + [f'Группа {i}' for i in range(1, 17)]
+        rows = []
 
-        schedule.to_csv(path, sep=';', index=False)
+        for day_str in ["1", "2", "3", "4", "5", "6"]:
+            day_name = WEEK_DAYS[day_str]
 
-        print(schedule)
-        print(schedule.count().sum() - 48 * 2)
-        print(f"CSV файл '{path}' успешно создан!")
+            for lesson_number in range(1, 9):
+                row_data = {
+                    "day": day_name,
+                    "lesson": lesson_number,
+                }
 
+                for col in columns[2:]:
+                    row_data[col] = ""
+
+                for group_key, days_dict in self.dict_table.items():
+                    if group_key in GROUP_COLUMN_NAME:
+                        col_name = GROUP_COLUMN_NAME[group_key]
+                    else:
+                        continue
+
+                    if day_str not in days_dict:
+                        continue
+
+                    lessons_list = days_dict[day_str]
+                    found_lesson = None
+                    for l in lessons_list:
+                        if l.get('pair') == lesson_number:
+                            found_lesson = l
+                            break
+
+                    if found_lesson:
+                        row_data[col_name] = found_lesson['teacher']
+
+
+                rows.append(row_data)
+
+        df = pd.DataFrame(rows, columns=columns)
+
+        df.to_csv(filename, sep=';', index=False, encoding='utf-8-sig')
+        print(f"Сформирован CSV-файл: {filename}")
